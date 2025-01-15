e469403c60d821ef4693e8ed3ba5b1bdd24c35b1
pChoudhary02582 <167977931+pChoudhary02582@users.noreply.github.com>
2025-01-15 20:48:00 +0530

    Update Sample1.java

--- src/main/java/org/example/Sample1.java
+++ src/main/java/org/example/Sample1.java
@@ -3,5 +3,6 @@ package org.example;
 public class Sample1 {
     public static void main(String[] args) {
         System.out.println("Sample test msg for github");
+        //something has been change now.
     }
 }
